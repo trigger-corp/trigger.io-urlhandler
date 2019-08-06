4cf5dfafce3d570720b42bd8a77ee924002a197c
Antoine van Gelder <antoine@trigger.io>
2019-08-06 11:23:09 +0200

    Use https for tests

--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -1,14 +1,13 @@
 module("forge.urlhandler");
 
 if (forge.is.mobile()) {
-	asyncTest("urlhandler", 1, function() {
-		forge.urlhandler.urlLoaded.addListener(function () {
-			ok(true, "Event recieved");
-			start();
-		});
-		askQuestion("Click the link on the webpage that opens", { Ok: function () {
-			window.location.href = "http://ops.trigger.io/75d92dce/tests/link.htm";
-		}});
-	});
+    asyncTest("urlhandler", 1, function() {
+        forge.urlhandler.urlLoaded.addListener(function () {
+            ok(true, "Event recieved");
+            start();
+        });
+        askQuestion("Click the link on the webpage that opens", { Ok: function () {
+            window.location.href = "https://ops.trigger.io/75d92dce/tests/link.htm";
+        }});
+    });
 }
-
